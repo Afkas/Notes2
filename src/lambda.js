5816bab67e6e0c8144a29c3a3c6d9319102be0ac
AfkasGenting <akin.palmer@gentinguk.com>
2022-03-04 15:51:44 +0000

    Adding a storage stack

--- src/lambda.js
+++
@@ -1,7 +0,0 @@
-export async function handler(event) {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hi there my name is Afkas and I dont like "Hello World" applications. \n ${event.requestContext.time}`,
-  };
-}
